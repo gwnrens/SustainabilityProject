9bb02b9ded73b37cd3a9519387eb1f08f9afcd23
Singyu <Singyu.Liu@student.ap.be>
2024-01-04 18:31:46 +0100

    Add factory build script platform

---
+++ Assets/Singyu/Scripts/ActivateFactory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFactory : MonoBehaviour
+{
+    public GameObject coalFactory; 
+    public AudioClip activationSound; 
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = activationSound;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            coalFactory.SetActive(true); 
+            audioSource.Play(); 
+        }
+    }
+}
