745a9d85ecb67ffe8817cae85e412036d3c77061
seppe.f <Seppe.faster@student.kdg.be>
2024-01-21 18:27:22 +0100

    Adding all scripts + changes

--- Assets/Seppe/Scripts/Flashlight.cs
+++ Assets/Seppe/Scripts/Flashlight.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public Light flashlight;
-    private bool isFlashlightOn = false;
+    public bool isFlashlightOn = false;
     private bool canToggle = true;
-    public float toggleCooldown = 0.5f; 
+    public float toggleCooldown = 0.5f;
+    
+    //referentie naar audio scource component en de audio clips die afgespeeld moeten worden.
+    public AudioSource audioSource;
+    public AudioClip flashlightOnSound;
+    public AudioClip flashlightOffSound;
 
     void Update()
     {
+        //spatiebalk is ingedrukt?
         if (Input.GetKeyDown(KeyCode.Space) && canToggle)
         {
+            //Zaklamp staat omkeren aan word uit en uit word aan.
             ToggleFlashlight();
+            //Coroutine om over meerdere frames een cooldown te hebben dit om snel (spam) schakelen te voorkomen.
             StartCoroutine(StartCooldown());
         }
     }
 
     void ToggleFlashlight()
     {
+        //Zaklamp veranderen
         isFlashlightOn = !isFlashlightOn;
-
+        //Light component van de flashlight veranderen
         flashlight.enabled = isFlashlightOn;
+
+        //Afspelen van de sound effects
+        if (audioSource != null)
+        {
+            if (isFlashlightOn && flashlightOnSound != null)
+            {
+                audioSource.PlayOneShot(flashlightOnSound);
+            }
+            else if (!isFlashlightOn && flashlightOffSound != null)
+            {
+                audioSource.PlayOneShot(flashlightOffSound);
+            }
+        }
     }
 
-    System.Collections.IEnumerator StartCooldown()
+    // Cooldown zodat flashlight niet mega snel aan en uit gaat (fix voor bug omdat zonder cooldown wnr je op spatie drukte de flashlight al 20 keer in 1 seconde aan en uit ging.)
+    IEnumerator StartCooldown()
     {
         canToggle = false;
         yield return new WaitForSeconds(toggleCooldown);
---
+++ Assets/Seppe/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button pauseButton;
+    public Button resumeButton;
+
+    void Start()
+    {
+        //geen pause menu tijdens start van het spel
+        pauseMenu.SetActive(false);
+
+        // Luisteraars toevoegen voor het pauzeren en hervatten van het spel
+        pauseButton.onClick.AddListener(PauseGame);
+        resumeButton.onClick.AddListener(ResumeGame);
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void PauseGame()
+    {
+        // Pauzemenu activeren
+        pauseMenu.SetActive(true);
+        //Tijd freezen zorgt dat er niks meer gebeurt van logica of physics in de game
+        Time.timeScale = 0f;
+
+        //Resume & pauze knoppen
+        pauseButton.interactable = false;
+        resumeButton.interactable = true;
+    }
+
+    public void ResumeGame()
+    {
+        //pause scherm verdwijnt
+        pauseMenu.SetActive(false);
+        // resume tijd
+        Time.timeScale = 1f;
+
+        pauseButton.interactable = true;
+
+        resumeButton.interactable = false;
+    }
+}
