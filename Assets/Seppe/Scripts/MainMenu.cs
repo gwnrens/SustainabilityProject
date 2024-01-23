8a86576842a885643bd1e63417c3430d8ccd43e7
seppe.f <Seppe.faster@student.kdg.be>
2024-01-23 13:36:19 +0100

    adding fixes

--- Assets/Seppe/Scripts/MainMenu.cs
+++ Assets/Seppe/Scripts/MainMenu.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    int teller;
+    public GameObject eerstHulpText;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       if (teller == 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+       else
+        {
+            eerstHulpText.SetActive(true);
+            Invoke("HulpGame", 5f);
+        }
+    }
+
+    public void HulpGame()
+    {
+        eerstHulpText.SetActive(false);
+        teller++;
     }
 
     public void QuitGame()
